e7ca373e0d447626cec2b6905a5509f2fb5e4202
lumen019 <237933659+lumen019@users.noreply.github.com>
2025-12-25 03:12:36 -0700

    changed color behavior to begin with mousedown

--- script.js
+++ script.js
@@ -2,24 +2,39 @@ const gridContainer = document.querySelector("#gridContainer");
 let gridSize = 16;
 let totalCells = gridSize * gridSize;
 
+let isMouseDown = false;
+document.addEventListener("mousedown", event => {
+    isMouseDown = true;
+})
+document.addEventListener("mouseup", event => {
+    isMouseDown = false;
+})
+
 function createGrid () { 
     for (let i = 0; i < totalCells; i++){
-    const cell = document.createElement("div");
-    cell.classList.add("cell")
-    cell.style.width = `${100/gridSize}%`;
-    cell.style.height = `${100/gridSize}%`;
-    gridContainer.appendChild(cell);
-    cell.addEventListener("mouseover", event => {
-        cell.style.backgroundColor = "red";
-    })
-}
-}
+        const cell = document.createElement("div");
+        cell.classList.add("cell")
+        cell.style.width = `${100/gridSize}%`;
+        cell.style.height = `${100/gridSize}%`;
+        gridContainer.appendChild(cell);
 
+        cell.addEventListener("mousedown", event => {
+            cell.style.backgroundColor = "red";
+        })
+
+        cell.addEventListener("mouseover", event => {
+            if (isMouseDown === true) {
+                cell.style.backgroundColor = "red";
+            }
+        })
+    }
+}
 
 const button = document.querySelector("#prompt");
 button.addEventListener("click", event => {
         gridSize = prompt("Enter your desired grid size (1-100).");
-        if (gridSize > 100) {
+        gridSize = Number(gridSize)
+        if (gridSize < 1 || gridSize > 100 || isNaN(gridSize)) {
             alert("Your value is outside the accepted range.")
             gridContainer.innerHTML = " ";
             gridSize = 16;
