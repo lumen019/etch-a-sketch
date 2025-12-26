fbf6ca0627e1b107acc4803947c89a6f4796ba87
lumen019 <237933659+lumen019@users.noreply.github.com>
2025-12-26 01:25:27 -0700

    reset button now works depending on mode

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 const gridContainer = document.querySelector("#gridContainer");
 let gridSize = 16;
 let totalCells = gridSize * gridSize;
-
 let isMouseDown = false;
+let isDarkMode = false;
+
+
+
 gridContainer.addEventListener("mousedown", () => isMouseDown = true);
 document.addEventListener("mouseup", () => isMouseDown = false);
 gridContainer.addEventListener('dragstart', (e) => e.preventDefault());
+
+function randomRgb () {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+gridContainer.addEventListener("mousemove", (e) => {
+    if (!isMouseDown) return;
+    if (!e.target.classList.contains("cell")) return;
+
+    if (isDarkMode) {
+        e.target.style.backgroundColor = randomRgb();
+    } else {
+        e.target.style.backgroundColor = "aqua";
+    }   
+})
 
 function createGrid () { 
     totalCells = gridSize * gridSize;
@@ -21,12 +43,6 @@ function createGrid () {
         cell.addEventListener("mousedown", () => {
             cell.style.backgroundColor = "aqua";
         })
-
-        cell.addEventListener("mousemove", () => {
-            if (isMouseDown === true) {
-                cell.style.backgroundColor = "aqua";
-            }
-        })
     }
 }
 
@@ -41,24 +57,14 @@ function createRgbGrid () {
         cell.style.height = `${100/gridSize}%`;
         gridContainer.appendChild(cell);
 
-        const r = Math.floor(Math.random() * 256);
-        const g = Math.floor(Math.random() * 256);
-        const b = Math.floor(Math.random() * 256);
-
-        cell.addEventListener("mousedown", event => {
-            cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-        })
-
-        cell.addEventListener("mousemove", event => {
-            if (isMouseDown === true) {
-                cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-            }
+        cell.addEventListener("mousedown", () => {
+            cell.style.backgroundColor = randomRgb();
         })
     }
 }
 
-const button = document.querySelector("#prompt");
-button.addEventListener("click", () => {
+const resizeBtn = document.querySelector("#resizeBtn");
+resizeBtn.addEventListener("click", () => {
         gridSize = prompt("Enter your desired grid size (1-100).");
         gridSize = Number(gridSize)
         if (gridSize < 1 || gridSize > 100 || isNaN(gridSize)) {
@@ -77,10 +83,12 @@ resetBtn.addEventListener("click", () => {
 
 const darkMode = document.querySelector("#darkMode");
 darkMode.addEventListener ("click", () => {
-    const bodyElement = document.body;
-    bodyElement.style.backgroundColor = "#121212"
+    isDarkMode = true;
+    document.body.style.backgroundColor = "#121212"
     gridContainer.style.backgroundColor = "#2c2c2c"
     createRgbGrid ();
 })
+
+const lightMode = document.querySelector("#lightMode");
 
 createGrid();
